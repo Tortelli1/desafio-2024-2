8e52e91c403ddfaba32c5d6c6a1c5ed363c78bac
Marco Tortelli <marcopazinitortelli@gmail.com>
2024-11-14 12:35:24 -0300

    Primeiro commit

---
+++ desafio-2024-2/src/main/java/br/edu/unoesc/Desafio20242Application.java
@@ -0,0 +1,13 @@
+package br.edu.unoesc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio20242Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio20242Application.class, args);
+	}
+
+}
