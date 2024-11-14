75503604ad306cd3e4662c4372d1b1145954edf5
Marco Tortelli <marcopazinitortelli@gmail.com>
2024-11-14 12:59:35 -0300

    Subindo projeto novamente

---
+++ src/test/java/br/edu/unoesc/Desafio20242ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unoesc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio20242ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
