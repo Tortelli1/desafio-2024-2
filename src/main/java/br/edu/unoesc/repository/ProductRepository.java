6937650d5ba309655e2eed50487ea8b6e7cf5530
Marco Tortelli <marcopazinitortelli@gmail.com>
2024-11-15 10:55:49 -0300

    Configurando classe CategoryRepository

--- src/main/java/br/edu/unoesc/repository/ProductRepository.java
+++ src/main/java/br/edu/unoesc/repository/ProductRepository.java
@@ -1,11 +1,9 @@
 package br.edu.unoesc.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import br.edu.unoesc.model.Product;
 
-@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer>{
 
 }
