63f32b15b99f4cea69fe6a91ca663e190398f8f9
Marco Tortelli <marcopazinitortelli@gmail.com>
2024-11-15 10:38:08 -0300

    Criação e estruturaçã das Models Brand e Category

--- src/main/java/br/edu/unoesc/model/Brand.java
+++ src/main/java/br/edu/unoesc/model/Brand.java
@@ -1,5 +1,72 @@
 package br.edu.unoesc.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "BRAND")
 public class Brand {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "brand_name")
+	private String name;
+	
+	@Column(name = "mail")
+	private String mail;
+	
+	@Column(name = "phone")
+	private Long phone;
+	
+	@Column(name = "brand_status")
+	private Boolean active;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public Long getPhone() {
+		return phone;
+	}
+
+	public void setPhone(Long phone) {
+		this.phone = phone;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+	
+	
 
 }
